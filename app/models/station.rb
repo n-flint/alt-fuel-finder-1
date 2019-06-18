883ece04aae3c0961f7400ecb834531cb445857f
Noah Flint <noah.flint24@gmail.com>
2019-06-18 16:42:06 -0600

    added methods for attributes

--- app/models/station.rb
+++ app/models/station.rb
@@ -1,8 +1,22 @@
 class Station
+  attr_reader :name,
+              :street_address,
+              :city,
+              :state,
+              :zip,
+              :fuel_type,
+              :distance,
+              :access_times
 
   def initialize(station)
-    # require 'pry'; binding.pry
-
+    @name = station['station_name']
+    @street_address = station['street_address']
+    @city = station['city']
+    @state = station['state']
+    @zip = station['zip']
+    @fuel_type = station['fuel_type_code']
+    @distance = station['distance']
+    @access_times = station['access_days_time']
   end
 
 
