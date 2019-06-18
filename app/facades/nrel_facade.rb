03f769fe28b021fceb5ed21609f6ab17c3803034
Noah Flint <noah.flint24@gmail.com>
2019-06-18 16:24:45 -0600

    recfactored with ENV[NREL_KEY]

--- app/facades/nrel_facade.rb
+++ app/facades/nrel_facade.rb
@@ -5,14 +5,15 @@ class NrelFacade
   end
 
   def station_count
-    conn = Faraday.get("https://developer.nrel.gov/api/alt-fuel-stations/v1/nearest.json?api_key=xwzOg8efd6vKDqgsQEo9dx1CEE6QJ65ENaHKJQw9&location=#{@zip}&fuel_type=ELEC,LPG&access=public&status=E")
+    conn = Faraday.get("https://developer.nrel.gov/api/alt-fuel-stations/v1/nearest.json?api_key=#{ENV['NREL_KEY']}&location=#{@zip}&fuel_type=ELEC,LPG&access=public&status=E")
     JSON.parse(conn.body)['total_results']
   end
 
   def stations
-    conn = Faraday.get("https://developer.nrel.gov/api/alt-fuel-stations/v1/nearest.json?api_key=xwzOg8efd6vKDqgsQEo9dx1CEE6QJ65ENaHKJQw9&location=#{@zip}&fuel_type=ELEC,LPG&access=public&status=E")
+    conn = Faraday.get("https://developer.nrel.gov/api/alt-fuel-stations/v1/nearest.json?api_key=#{ENV['NREL_KEY']}&location=#{@zip}&fuel_type=ELEC,LPG&access=public&status=E")
+
     stations = JSON.parse(conn.body)['fuel_stations'].first(15)
-    
+
     stations.map do |station|
       Station.new(station)
     end
