725cbe4420033f01725f1794e63ef3d68cdbd571
Noah Flint <noah.flint24@gmail.com>
2019-06-18 17:09:01 -0600

    added stations method and private json method

--- app/services/nrel_service.rb
+++ app/services/nrel_service.rb
@@ -5,7 +5,16 @@ class NrelService
   end
 
   def station_count
-    JSON.parse(conn.body)['total_results']
+    json['total_results']
+  end
+
+  def stations(limit = 15)
+    stations = json['fuel_stations'].first(limit)
+
+    stations.map do |station|
+      Station.new(station)
+    end
+
   end
 
   private
@@ -13,4 +22,7 @@ class NrelService
       Faraday.get("https://developer.nrel.gov/api/alt-fuel-stations/v1/nearest.json?api_key=#{ENV['NREL_KEY']}&location=#{@zip}&fuel_type=ELEC,LPG&access=public&status=E")
     end
 
+    def json
+      JSON.parse(conn.body)
+    end
 end
